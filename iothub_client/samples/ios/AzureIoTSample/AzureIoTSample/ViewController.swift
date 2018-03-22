1944c769695946cac459fd2e5c2c8292e2875972
Roy Sprowl <v-royspr@microsoft.com>
2018-03-21 18:43:47 -0700

    ...

--- iothub_client/samples/ios/AzureIoTSample/AzureIoTSample/ViewController.swift
+++ iothub_client/samples/ios/AzureIoTSample/AzureIoTSample/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController, logTarget {
     
     func addLogEntry(_ logEntry: String!) {
-        
+        DispatchQueue.main.async {
+            //[log_target addLogEntry:temp];
+        }
     }
     
+    @IBOutlet weak var txtLogDisplay: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
